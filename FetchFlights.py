02f878ff54bb0d250cfecd9a741fda7d24b3f6f6
Arjan <aguglani@umich.edu>
2020-12-13 16:00:51 -0500

    Documentation

--- FetchFlights.py
+++ FetchFlights.py
@@ -3,6 +3,7 @@ import unittest
 import os
 import requests
 import sqlite3
+import re
 
 
 # FetchFlights.py
@@ -14,8 +15,14 @@ import sqlite3
 def databaseValidation(cur):
 
     try:
+
+        counta = 0
         cur.execute("SELECT * FROM Flights")
-        return cur.rowcount()
+
+        for flight in cur:
+            counta += 1
+
+        return counta
 
     except:
         return 0
@@ -26,6 +33,21 @@ def setUpDatabase(db_name):
     cur = conn.cursor()
     return cur, conn
 
+def getAirportData(code):
+
+    print("Getting Airport Info from FlightAware about " + code)
+    data = requests.get("http://traveltimeiosapp:b34cb08f23579a0812280da25b76aee4e47bac16@flightxml.flightaware.com/json/FlightXML3/AirportInfo?airport_code=" + code)
+    
+    try:
+
+        jsonData = json.loads(data.text)["AirportInfoResult"]
+        return (str(jsonData.get("airport_code", code)), float(jsonData.get("longitude", 0.0)), float(jsonData.get("latitude", 0.0)), str(jsonData.get("state", "--")), str(jsonData.get("country_code", "--")))
+
+    except:
+
+        print("Uh-Oh... we are having some trouble connecting to FlightAware")
+        return None
+
 # Get 25 Arrivals from FlightAware Airport Boards API
 # This function takes in how many recent arrivals to offset from and returns the arrivals board for the DTW airport
 
@@ -34,44 +56,42 @@ def flightBoardDTW(offset):
     print("Getting Flights from FlightAware")
     data = requests.get("http://traveltimeiosapp:b34cb08f23579a0812280da25b76aee4e47bac16@flightxml.flightaware.com/json/FlightXML3/AirportBoards?howMany=25&offset=" + str(offset) + "&airport_code=KDTW&include_ex_data=true&type=arrivals&filter=airline")
 
-    #try:
-    return json.loads(data.text)["AirportBoardsResult"]["arrivals"]["flights"]
+    try:
+        return json.loads(data.text)["AirportBoardsResult"]["arrivals"]["flights"]
 
- #   except:
+    except:
 
-        #print("Uh-Oh... we are having some trouble connecting to FlightAware")
-       # return None
+        print("Uh-Oh... we are having some trouble connecting to FlightAware")
+        return None
 
 def main():
 
     cur, conn = setUpDatabase("data.db")
     validateDatabaseCount = databaseValidation(cur)
+    print(validateDatabaseCount)
 
     if(validateDatabaseCount == 0):
-       cur.execute("CREATE TABLE IF NOT EXISTS Flights (flightNumber INTEGER PRIMARY KEY, origin TEXT, PAXCount INTEGER)")
-       cur.execute("CREATE TABLE IF NOT EXISTS Locals (code TEXT PRIMARY KEY, lng DOUBLE, lat DOUBLE, state TEXT)")
+
+       cur.execute("DROP TABLE IF EXISTS Flights")
+       cur.execute("DROP TABLE IF EXISTS Locals")
+       cur.execute("DROP TABLE IF EXISTS Corona")
+       cur.execute("CREATE TABLE IF NOT EXISTS Flights (flightNumber TEXT PRIMARY KEY, origin TEXT, PAXCount INTEGER)")
+       cur.execute("CREATE TABLE IF NOT EXISTS Locals (code TEXT PRIMARY KEY, lng DOUBLE, lat DOUBLE, state TEXT, countryCode TEXT)")
        cur.execute("CREATE TABLE IF NOT EXISTS Corona (state TEXT PRIMARY KEY, peoplePositiveNewCasesCt INTEGER, peopleNegativeNewCt INTEGER, peopleDeathCt INTEGER)")
 
-    grabFlights = flightBoardDTW(validateDatabaseCount)
+    grabFlights = flightBoardDTW(validateDatabaseCount+2)
 
     for flight in grabFlights:
-        print(flight)
-
+        #print(flight)
         print((flight["ident"], flight["origin"]["code"], (int(flight.get("seats_cabin_business", "0")) + int(flight.get("seats_cabin_coach", "0")))))
-
-        #cur.execute("INSERT INTO Flights (flightNumber, origin, PAXCount) VALUES (?, ?, ?) ", (flight["ident"], flight["origin"]["code"], (int(flight.get("seats_cabin_business", "0")) + int(flight.get("seats_cabin_coach", "0")))))
+        cur.execute("INSERT OR IGNORE INTO  Flights (flightNumber, origin, PAXCount) VALUES (?, ?, ?) ", (str(flight["ident"]), str(flight["origin"]["code"]), (int(flight.get("seats_cabin_business", "0")) + int(flight.get("seats_cabin_coach", "0")))))
+        cur.execute("INSERT OR IGNORE INTO Locals (code, lng, lat, state, countryCode) VALUES (?, ?, ?, ?, ?) ", getAirportData(str(flight["origin"]["code"])))
 
         # insert into flights table
         # insert airport data into Locals Table using data from an indvidual flight packet
         # use State Code to launch and get corona data and insert result into corona table if and only if the state is not already supported
 
-
-
     conn.commit()
-
-
-   
-
     conn.close()
     
 
